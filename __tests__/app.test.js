d735d857ca25f1581b79b191d0ba9d682c9ffaa8
Mariah <mariahschock@gmail.com>
2022-08-08 16:29:43 -0700

    create user test is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,34 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const testUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'usertest@me.com',
+  password: 'qwerty',
+};
+
+
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+ 
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(testUser);
+    const { firstName, lastName, email } = testUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
